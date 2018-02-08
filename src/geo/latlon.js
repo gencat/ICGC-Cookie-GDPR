eb8e645237878fc0b8fcf15ddd9b68f0f52a7c05
Isaac Besora Vilardaga <isaac@altersport.cat>
2018-02-08 14:35:36 +0100

    Initial commit

---
+++ src/geo/latlon.js
@@ -0,0 +1,77 @@
+// @flow
+'use strict';
+
+/**
+ * A `LatLon` object represents a given latitude and longitude coordinates.
+ *
+ * @param {number} lat Latitude, measured in degrees.
+ * @param {number} lon Longitude, measured in degrees.
+ * @example
+ * var ll = new LatLon(42.10376, 1.84584);
+ */
+class LatLon {
+    lat: number;
+    lon: number;
+
+    constructor(lat: number, lon: number) {
+
+        const areNumbers = !(isNaN(lat) || isNaN(lon));
+        if (areNumbers) {
+
+            this.lat = +lat;
+            this.lon = +lon;
+
+            const validRange = this.lat > -90 && this.lat < 90 &&
+                this.lon > -180 && this.lon < 180;
+
+            if (!validRange) {
+
+                throw new Error('Invalid LatLon value: Latitude must be between -90 and 90 and Longitude must be between -180 and 180');
+
+            }
+
+        } else {
+
+            throw new Error(`Invalid LatLon object: (${lat}, ${lon})`);
+
+        }
+
+    }
+
+    /**
+     * Set the latitude
+     *
+     * @param {number} lat
+     * @returns {LatLon} `this`
+     */
+    setLatitude(lat: number) {
+        this.lat = lat;
+        return this;
+    }
+
+    /**
+     * Set the longitude
+     *
+     * @param {number} lon
+     * @returns {LatLon} `this`
+     */
+    setLongitude(lon: number) {
+        this.lon = lon;
+        return this;
+    }
+
+    /**
+     * Returns the LatLon object as a string.
+     *
+     * @returns {string} The coordinates as a string in the format `'LatLon(lat, lng)'`.
+     * @example
+     * var ll = new LatLon(42.10376, 1.84584);
+     * ll.toString(); //"LatLon(42.10376, 1.84584)"
+     */
+    toString() {
+        return `LatLon(${this.lat}, ${this.lon})`;
+    }
+
+}
+
+module.exports = LatLon;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+// @flow
+'use strict';
+
+const version: string = require('../package.json').version;
+const LatLon = require('./geo/latlon');
+const LatLonBounds = require('./geo/latlonBounds');
+
+module.exports = {
+    version,
+    LatLon,
+    LatLonBounds
+};
+
+/**
+ * The version of the project in use as specified in `package.json`,
+ * `CHANGELOG.md`, and the GitHub release.
+ *
+ * @var {string} version
+ */
